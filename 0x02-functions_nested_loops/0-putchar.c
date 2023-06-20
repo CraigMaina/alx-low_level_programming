28805e675b56eb98fd517254b148ab60aec9e653
CraigMaina <craigmaina@gmail.com>
2023-06-20 13:28:08 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+  int mstari = 0;
+  while (mstaari < 5)
+    {
+      printf("_putchar");
+	mstari++;
+    }
+
+  return 0;
+}
